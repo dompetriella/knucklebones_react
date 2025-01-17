da86389f4b3f2e858dd6a85d869985212c181305
Dominic Petriella <dompetriella@gmail.com>
2025-01-17 17:36:34 -0500

    missed one

--- src/pages/HowToPlayPage.tsx
+++ src/pages/HowToPlayPage.tsx
@@ -3,7 +3,6 @@ import { BackButton } from "../components/utility/BackButton";
 import { DiceData } from "../models/DiceData";
 import { Player } from "../models/Player";
 import { PlayerDiceBlock } from "../components/playerArea/PlayerDiceBlock";
-import { Scoreboard } from "../components/statusBar/Scoreboard";
 import { v4 as uuidv4 } from "uuid";
 import { characterDataList } from "../global/characterData";
 import useScreenWidth from "../hooks/useScreenWidth";
